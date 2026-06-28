0879aff1a016c07ed320c5b2173ce97428135645
Gildean Morais <gildean.m098@gmail.com>
2026-06-28 16:29:32 -0300

    feat: vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         // App do Cliente (Mobile)
-        main: resolve(__dirname, 'index.html'), 
+        main: resolve(__dirname, 'index.html'),
+
+        // App do Admin   
+        kds: resolve(__dirname, 'src/pages/admin/kds.html'),
+        teamManagement: resolve(__dirname, 'src/pages/admin/team-management.html'),
+        orders: resolve(__dirname, 'src/pages/admin/orders.html'),
+        cadastroFuncionario: resolve(__dirname, 'src/pages/admin/employee-form.html'),
+        menuManagement: resolve(__dirname, 'src/pages/admin/menu-management.html')
       }
     }
   }
